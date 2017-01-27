48278bdeffe33d2cb5ff22bc60a8b019be8c8286
temkasmirnov <5725757@mail.ru>
2017-01-27 12:55:20 +0400

    Add files via upload

---
+++ laba7_OS.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <semaphore.h>
+
+pthread_rwlock_t lock;
+
+//используются системные вызовы для работы с файлами
+
+void* writing()
+{
+	time_t rawtime; 
+	struct tm *timeinfo;
+	int file;
+	int num_str = 1;
+	char str[100];
+
+	while(1)
+	{
+		pthread_rwlock_wrlock(&lock);
+			
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+
+		//открываем/создаем файл в режиме добавления, только на запись
+		file = open("time.txt", O_CREAT|O_WRONLY|O_APPEND, 0700);
+		sprintf(str, "string #%d:\t%d:%d:%d\n", num_str,timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+		write(file, str, strlen(str));
+		num_str++; 		
+		
+		pthread_rwlock_unlock(&lock);
+		sleep(1);
+	}
+	pthread_exit(0);
+}
+
+void* reading()
+{
+	int file;
+	int str_read = 0;
+	int str_write = 0;
+	char str[100];
+	while(1)
+	{
+		pthread_rwlock_rdlock(&lock);
+		
+		//открываем/создаем файл только на чтение
+		file = open("time.txt", O_CREAT|O_RDONLY, 0700);
+		
+		while ((str_read = read(file, str, 128)) > 0)
+		{
+		    str_write = write (1, str, str_read);
+		}	
+		printf("&&&&&&&&&&&&&&&&&&&&\n");
+		
+		pthread_rwlock_unlock(&lock);
+		sleep(5);
+	}
+	pthread_exit(0);  
+}
+
+int main (int argc, char* argv[])
+{
+	pthread_t writer;
+	pthread_t readers [10];
+
+	int i;
+	
+	pthread_rwlock_init(&lock, NULL);
+
+	if(pthread_create(&writer, NULL, writing, NULL)<0)
+	{	
+		printf("ERROR: create writer thread false\n");
+		return -1;
+	}
+
+	for (i = 0; i<10; ++i)
+	{
+		if(pthread_create(&readers[i], NULL, reading, NULL)<0)
+		{	
+			printf("ERROR: create reading thread %d false\n", i);
+			return -1;
+		}
+	}
+	
+	if(pthread_join(writer, NULL)<0)
+	{	
+		printf("ERROR: joining writer thread false\n");
+		return -1;
+	}
+
+	for (i = 0; i<10; ++i)
+	{
+		if(pthread_join(readers[i], NULL)<0)
+		{	
+			printf("ERROR: joining reading thread %d false\n", i);
+			return -1;
+		}
+	}
+
+	pthread_rwlock_destroy(&lock);
+	
+	return 0;
+}
